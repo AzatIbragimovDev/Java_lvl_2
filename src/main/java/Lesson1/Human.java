2404c80726d965a0b3915eb8701468c8fd052494
AzatIbragimov <azatibragimow@inbox.ru>
2021-07-06 19:02:33 +0300

    Add HW lesson_21 Java lvl_2

---
+++ src/main/java/Lesson1/Human.java
@@ -0,0 +1,27 @@
+package Lesson1;
+
+public class Human {
+    private String name;
+    private int range;
+    private double jumpHeight;
+    public boolean success = false;
+    public Human(String name, int range, double jumpHeight) {
+        this.name = name;
+        this.range = range;
+        this.jumpHeight = jumpHeight;
+
+    }
+    public boolean run(Track t) {
+        success = t.runDistance(range, name, t.getDistance());
+        if(success){
+        return true;}
+        else  return false;
+    }
+    public boolean jump(Wall w) {
+        success = w.leap(jumpHeight, name, w.getHeight());
+
+        if(success){
+            return true;}
+        else  return false;
+    }
+}
---
+++ src/main/java/Lesson1/Robot.java
@@ -0,0 +1,20 @@
+package Lesson1;
+
+public class Robot {
+    private String name;
+    private int range;
+    private double jumpHeight;
+    private boolean success = false;
+    public Robot(String name, int range, double jumpHeight) {
+        this.name = name;
+        this.range = range;
+        this.jumpHeight = jumpHeight;
+    }
+    public void run(Track t) {
+        success = t.runDistance(range, name, t.getDistance());
+    }
+    public void jump(Wall w) {
+        success = w.leap(jumpHeight, name, w.getHeight());
+    }
+
+}
